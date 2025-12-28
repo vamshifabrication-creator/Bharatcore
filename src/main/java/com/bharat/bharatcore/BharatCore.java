03764b707dee881f8cad261f48713b8da814088c
vamshifabrication-creator <vamshi.fabrication@gmail.com>
2025-12-28 16:56:32 +0530

    Update BharatCore.java
    package com.bharat.bharatcore;
    
    import org.bukkit.command.Command;
    import org.bukkit.command.CommandSender;
    import org.bukkit.plugin.java.JavaPlugin;
    
    public class BharatCore extends JavaPlugin {
    
        @Override
        public void onEnable() {
            getLogger().info("BharatCore enabled successfully!");
        }
    
        @Override
        public void onDisable() {
            getLogger().info("BharatCore disabled.");
        }
    
        @Override
        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
            if (command.getName().equalsIgnoreCase("bharat")) {
                sender.sendMessage("§aBharat SMP Core is running!");
                return true;
            }
            return false;
        }
    }

--- src/main/java/com/bharat/bharatcore/BharatCore.java
+++ src/main/java/com/bharat/bharatcore/BharatCore.java
@@ -1,5 +1,7 @@
 package com.bharat.bharatcore;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BharatCore extends JavaPlugin {
@@ -13,5 +15,13 @@ public class BharatCore extends JavaPlugin {
     public void onDisable() {
         getLogger().info("BharatCore disabled.");
     }
-}
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("bharat")) {
+            sender.sendMessage("§aBharat SMP Core is running!");
+            return true;
+        }
+        return false;
+    }
+}
